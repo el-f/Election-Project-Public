cf45f495ef7b3de1bd0466339bef65a8b07d06ba
E. Fine <39451680+Elfein7Night@users.noreply.github.com>
2020-08-21 10:28:53 +0300

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -6,12 +6,11 @@ import javafx.stage.Stage;
 
 public class Program extends Application {
 
-    @SuppressWarnings("unused") //controller is used.
     @Override
     public void start(Stage stage) throws Exception {
         Election election = Election.initDefault();
         View view = new View(stage);
-        Controller controller = new Controller(election, view);
+        new Controller(election, view);
     }
 
     public static void main(String[] args) {
